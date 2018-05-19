904cea408bc0be405ab88164e4920d4c99be940a
nisar <nisar@gmail.com>
2018-05-19 17:49:56 +0530

    lint

--- src/app/app.routing.guard.ts
+++ src/app/app.routing.guard.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Router } from "@angular/router";
 import * as config from '../assets/appsettings.json';
 @Injectable()
 export class AppRoutingGuard implements CanActivate {
     constructor(private router: Router) { };
 
     canActivate() {
-        if(config.default.enableAuth){
+        if (config.default.enableAuth) {
             const isAuth = !!localStorage.getItem('auth_app_token');
             if (isAuth) {
                 return true;
@@ -17,9 +16,9 @@ export class AppRoutingGuard implements CanActivate {
                 this.router.navigate(['auth']);
                 return false;
             }
-        }else{
+        } else {
             return true;
         }
-        
+
     }
 }
